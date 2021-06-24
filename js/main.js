a1112534fbf28b6b2987de6924006de19b1c013a
takashiono2 <takashiono710@gmail.com>
2021-06-24 23:42:18 +0900

    todoアプリ

--- js/main.js
+++ js/main.js
@@ -6,10 +6,26 @@
     data: {
       newItem:'',
       todos:[
-        // {title: 'task1' , isDone: false},
-        // {title: 'task2' , isDone: false},
-        // {title: 'task3' , isDone: true}
+        {title: 'task1' , isDone: false},
+        {title: 'task2' , isDone: false},
+        {title: 'task3' , isDone: true}
       ]
+    },
+    watch: {
+      // todos: function() {
+      //   localStorage.setItem('todos', JSON.stringify(this.todos));
+      //   alert('Data saved!');
+      // }
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+          // alert('Data saved!');
+        },
+        deep: true
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods:{
       addItem: function(){
@@ -24,6 +40,26 @@
         if(confirm('are you sure?')){
           this.todos.splice(index,1);
         }
+      },
+      purge: function() {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        // this.todos = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // });
+        this.todos = this.remaining;
+      }
+    },
+    computed: {
+      remaining: function() {
+        // var items = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // });
+        // return items.length;
+        return this.todos.filter(function(todo) {
+          return !todo.isDone;
+        });
       }
     }
   })
