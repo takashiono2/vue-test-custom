d58fd76bab60e96ac8051eae1f5d40bc482a8cb9
takashiono2 <takashiono710@gmail.com>
2021-07-04 18:22:13 +0900

    localStorageまで

--- js/main.js
+++ js/main.js
@@ -6,10 +6,22 @@
     data: {
       newItem: '', 
       todos: [
-        {title: 'task1',isDone: false},
-        {title: 'task2',isDone: false},
-        {title: 'task3',isDone: true}
+        // {title: 'task1',isDone: false},
+        // {title: 'task2',isDone: false},
+        // {title: 'task3',isDone: true}
       ]
+    },
+    watch: {
+      todos: {
+        handler: function(){
+          localStorage.setItem('todos',JSON.stringify(this.todos))
+          // alert('data saved!')
+        },
+        deep: true
+      }
+    },
+    mounted: function(){
+      this.todos = JSON.parse(localStorage.getItem('todos'))
     },
     methods: {
       addItem: function(){
@@ -24,7 +36,27 @@
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1);
         }
+      },    
+      purge: function(){//チェックがついているものを削除したい
+        if (!confirm('delete finished?')) {
+          return;
+        }  
+        this.todos = this.todos.filter(function(todo){
+          return !todo.isDone;//終わってないものだけ残す            
+        });
+      }
+    },
+    computed: {
+      remaining: function(){
+        var items = this.todos.filter(function(todo){
+          if (todo.isDone === false){
+            return !todo.isDone;//falseを返す
+          }else{
+            return null;
+          }
+        });
+        return items.length;
       }
     }
-  }) 
+  });
 })();
